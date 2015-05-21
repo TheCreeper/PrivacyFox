a5636a151ffcccd4ae145e9362b16a3add62f5d2
TheCreeper <dc0@firebit.co.uk>
2015-05-21 13:22:37 +0100

    first commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# PrivacyFox
+PrivacyFox attemts to disable/enable various features in firefox in order to enhance privacy. A list of all settings can be found in [bootstrap.js](bootstrap.js) along with a description of each one.
+
+## building the XPI
+```
+zip privacyfox.xpi bootstrap.js install.rdf
+```
---
+++ bootstrap.js
@@ -0,0 +1,110 @@
+const {classes : Cc, interfaces : Ci, utils : Cu} = Components;
+Cu.import('resource://gre/modules/Services.jsm');
+
+var privacyPrefs =
+    [
+
+      // Disable referrer headers.
+      {pref : "network.http.sendRefererHeader", set : 0, type : "int"},
+
+      // Disable referrer headers between https websites.
+      {
+	pref : "network.http.sendSecureXSiteReferrer",
+	set : false,
+	type : "bool"
+      },
+
+      // This is Mozilla’s new built in tracking protection.
+      {pref : "privacy.trackingprotection.enabled", set : true, type : "bool"},
+
+      // Disables geolocation.
+      {pref : "geo.enabled", set : false, type : "bool"},
+
+      // Disable Google Safe Browsing and phishing protection. Security risk,
+      // but privacy improvement.
+      {pref : "browser.safebrowsing.enabled", set : false, type : "bool"},
+
+      // Disable Google Safe Browsing malware checks. Security risk, but privacy
+      // improvement.
+      {
+	pref : "browser.safebrowsing.malware.enabled",
+	set : false,
+	type : "bool"
+      },
+
+      // Disable that websites can get notifications if you copy, paste, or cut
+      // something from a web page, and it lets them know which part of the page
+      // had been selected.
+      {pref : "dom.event.clipboardevents.enabled", set : false, type : "bool"},
+
+      // Disables website control over rightclick context menu.
+      {pref : "dom.event.contextmenu.enabled", set : false, type : "bool"},
+
+      // Disables firefox logging geolocation requests.
+      {pref : "geo.wifi.logging.enabled ", set : false, type : "bool"},
+
+      // Number of cached DNS entries. Lower number = More requests but less
+      // data stored.
+      {pref : "network.dnsCacheEntries", set : 100, type : "int"},
+
+      // Time DNS entries are cached in seconds.
+      {pref : "network.dnsCacheExpiration", set : 60, type : "int"},
+
+      // Disables saving of formdata.
+      {pref : "browser.formfill.enable", set : false, type : "bool"},
+
+      // The attribute would be useful for letting websites track visitors’
+      // clicks.
+      {pref : "browser.send_pings", set : false, type : "bool"},
+
+      // WebSockets is a technology that makes it possible to open an
+      // interactive communication session between the user's browser and a
+      // server.
+      {pref : "network.websocket.enabled", set : false, type : "bool"},
+
+      // WebGL is a potential security risk.
+      {pref : "webgl.disabled", set : false, type : "bool"}
+];
+
+function startup(data, reason) { setPrivacyPrefs(); }
+
+function shutdown(data, reason) { resetPrivacyPrefs(); }
+
+function install(data, reason) { setPrivacyPrefs(); }
+
+function uninstall(data, reason) { resetPrivacyPrefs(); }
+
+function setPrivacyPrefs()
+{
+	for (i = 0; i < privacyPrefs.length; i++) {
+
+		switch (privacyPrefs[i]["type"]) {
+
+			case "int":
+				Services.prefs.setIntPref(
+				    privacyPrefs[i]["pref"],
+				    privacyPrefs[i]["set"]);
+				break;
+			case "bool":
+				Services.prefs.setBoolPref(
+				    privacyPrefs[i]["pref"],
+				    privacyPrefs[i]["set"]);
+				break;
+			case "char":
+				Services.prefs.setCharPref(
+				    privacyPrefs[i]["pref"],
+				    privacyPrefs[i]["set"]);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+function resetPrivacyPrefs()
+{
+	for (i = 0; i < privacyPrefs.length; i++) {
+
+		Services.prefs.clearUserPref(privacyPrefs[i]["pref"]);
+	}
+}
